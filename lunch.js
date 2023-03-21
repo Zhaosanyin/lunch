1e4450b38e171d51d6cd8d6c3a016bf8ebe6d364
zhaosanyin <zhaosan.yin@gmail.com>
2023-03-21 21:05:35 +0000

    lunch

---
+++ lunch.js
@@ -0,0 +1,17 @@
+/*I'm hungry and I have 20 minutes for lunch.
+I don't know what to do!
+--
+I'm hungry and I have 50 minutes for lunch.
+I don't know what to do!
+--
+I'm not hungry and I have 30 minutes for lunch.
+I don't know what to do!
+--
+I'm hungry and I have 15 minutes for lunch.
+I don't know what to do! */
+
+function whatToDoForLunch(hungry, availableTime){
+  console.log("hungry is", hungry);
+  console.log("available time is", availableTime);
+}
+console.log(whatToDoForLunch(yes, 30));
