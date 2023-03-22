a4acab22635564757074bf5312bf1031a645342a
zhaosanyin <zhaosan.yin@gmail.com>
2023-03-22 15:31:31 +0000

    condition lunch

--- loopy.js
+++ loopy.js
@@ -13,3 +13,18 @@ for (let loop_n = 100; loop_n <= 200; loop_n += 1 ) {
     console.log (loop_n);
   }
 }
+
+const isHungry = true;
+const availableTime = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
--- lunch.js
+++ lunch.js
@@ -1,3 +1,4 @@
+
 /*I'm hungry and I have 20 minutes for lunch.
 I don't know what to do!
 --
@@ -8,10 +9,32 @@ I'm not hungry and I have 30 minutes for lunch.
 I don't know what to do!
 --
 I'm hungry and I have 15 minutes for lunch.
-I don't know what to do! */
+I don't know what to do! 
 
 function whatToDoForLunch(hungry, availableTime){
   console.log("hungry is", hungry);
   console.log("available time is", availableTime);
 }
 console.log(whatToDoForLunch(yes, 30));
+
+function whatToDoForLunch(hungry, availableTime) {
+  console.log("hungry is", hungry);
+  console.log("availableTime is", availableTime);
+}
+*/
+
+
+const hungry = true;
+
+function whatToDoForLunch(hungry, availableTime) {
+  if (availableTime <= 20) {
+    console.log("pick up a snack or grab something you have ready at home!");
+  } else if (availableTime <= 30) {
+    console.log("Maybe going outside isn't such a great idea…");
+    } else{
+    console.log("this is an intense program after all and you should probably reconsider.");
+      }
+
+  return;
+}
+console.log(whatToDoForLunch(hungry, 30));
