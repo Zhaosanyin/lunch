f13c65e769faa2bf61c12b66ca2a3aaa52afdb32
zhaosanyin <zhaosan.yin@gmail.com>
2023-03-22 02:54:45 +0000

    import text

--- lunch.js
+++ lunch.js
@@ -8,10 +8,22 @@ I'm not hungry and I have 30 minutes for lunch.
 I don't know what to do!
 --
 I'm hungry and I have 15 minutes for lunch.
-I don't know what to do! */
+I don't know what to do! 
 
 function whatToDoForLunch(hungry, availableTime){
   console.log("hungry is", hungry);
   console.log("available time is", availableTime);
 }
 console.log(whatToDoForLunch(yes, 30));
+
+function whatToDoForLunch(hungry, availableTime) {
+  console.log("hungry is", hungry);
+  console.log("availableTime is", availableTime);
+}
+*/
+let hungry = "";
+let availableTime = 
+
+if (hungry = "hungry") {
+  if (availableTime <15) 
+} 
