9b006983093ca24bae609e9d8ff6de7eda61b8e6
zhaosanyin <zhaosan.yin@gmail.com>
2023-03-26 22:17:55 +0000

    join and min

---
+++ joinList.js
@@ -0,0 +1,11 @@
+
+let joinList = function(arr) {
+  let strArr = "";
+  for (let i = 0; i < arr.length; i ++) {
+    strArr = strArr + arr [i] + ",";
+  }
+  return strArr;
+};
+
+const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
+console.log(joinList(conceptList));
