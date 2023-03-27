d774a8bc487ad70cbe3a7bf945e69e279e175dae
zhaosanyin <zhaosan.yin@gmail.com>
2023-03-27 14:50:24 +0000

    input by node

--- joinList.js
+++ joinList.js
@@ -1,17 +1,20 @@
 
 let joinList = function(arr) {
   let strArr = "";
-  for (let i = 0; i < arr.length; i ++) {
+  for (let i = 0; i < arr.length-1; i ++) {
+    if (i < arr.length) {
     strArr = strArr + arr [i] + ",";
-    }   
+    } else {
+      strArr = strArr + arr[i] + '.'};  
     //if (i =arr.length -1) {
     //  i = i+1;
     //  strArr =strArr +arr[i] + ".";
     //}
-  return strArr;
+  }
+    return strArr;
 };
 
 const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
 console.log(joinList(conceptList));
-console.log(conceptList);
+//console.log(conceptList);
 //joinList(conceptList);
