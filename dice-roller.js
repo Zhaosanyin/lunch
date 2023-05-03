6d370971f3c66fa3184738aad23550448e794dc0
Zhaosan Yin <zhaosan.yin@gmail.com>
2023-05-03 16:42:48 -0400

    test

--- dice-roller.js
+++ dice-roller.js
@@ -3,14 +3,14 @@ const args = process.argv.slice(2); //figure out the two arguments
 
 //console.log(args.slice(2,4)) checking the string status;
 
-const argsJ = args.map(Number); // string to number conversion
-const args1 =argsJ.join();
+const args1 = args.map(Number); // string to number conversion
+//const args1 =argsJ.join();
 var diceNumber = function (args1) {
   let newN = [ ];
   for(var i = 0; i<= args1-1; i ++){
     newN[i] = Math.ceil(Math.random() * args1)
 }
-return newN.join();
+return newN;
 };
 //console.log(args1);
 console.log(`dice-roller ${diceNumber(args1)}`);
